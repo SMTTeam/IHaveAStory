86bade4988b52224cfb12fd05f9fb720b4847a26
zs14 <1509403958@qq.com>
2018-12-13 20:39:40 +0800

    fix

---
+++ src/main/java/com/smtteam/smt/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.smtteam.smt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 类说明：测试Controller
+ * 创建者：Zeros
+ * 创建时间：2018-12-13 20:34
+ * 包名：com.smtteam.smt.controller
+ */
+
+@RestController
+public class TestController {
+
+    @GetMapping("")
+    public String test(){
+        return "This is a test";
+    }
+
+}
