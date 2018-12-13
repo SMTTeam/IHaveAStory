5d0c5b1805b37b2acebcadf09298cf5f0eecf853
zs14 <1509403958@qq.com>
2018-12-13 20:18:10 +0800

    init repository

---
+++ src/main/java/com/smtteam/smt/SmtApplication.java
@@ -0,0 +1,14 @@
+package com.smtteam.smt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmtApplication.class, args);
+    }
+
+}
+
