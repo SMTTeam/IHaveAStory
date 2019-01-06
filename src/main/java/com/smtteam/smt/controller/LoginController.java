fc6458c03dc61f8a9d8ea84ea9ed73ae8ed7eab9
WeiShixin <1204353094@qq.com>
2019-01-06 13:04:52 +0800

    删除resultVO 前面提交时忘记删除了，其实没有用到

--- src/main/java/com/smtteam/smt/controller/LoginController.java
+++ src/main/java/com/smtteam/smt/controller/LoginController.java
@@ -1,16 +1,11 @@
 package com.smtteam.smt.controller;
 
-import com.smtteam.smt.common.bean.LoginVO;
 import com.smtteam.smt.common.bean.ResultVO;
 import com.smtteam.smt.common.enums.ResultCode;
 import com.smtteam.smt.model.User;
 import com.smtteam.smt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 /**
  * createdBy: weishixin
