10848835edec7f0ef54ed36e2eaac6cfc5f9ae5c
Kimone <1799215066@qq.com>
2019-01-20 17:15:10 +0800

    fix bug

--- src/main/java/com/smtteam/smt/service/impl/ActivityServiceImpl.java
+++ src/main/java/com/smtteam/smt/service/impl/ActivityServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * created by Kimone
@@ -59,7 +60,8 @@ public class ActivityServiceImpl implements ActivityService {
 
     @Override
     public Activity getActivityById(int id) {
-        return activityDao.findById(id).get();
+        Optional<Activity> activity = activityDao.findById(id);
+        return activity.orElse(null);
     }
 
     @Override
--- src/test/java/com/smtteam/smt/service/impl/ActivityServiceImplTest.java
+++ src/test/java/com/smtteam/smt/service/impl/ActivityServiceImplTest.java
@@ -3,8 +3,12 @@ package com.smtteam.smt.service.impl;
 import com.smtteam.smt.SmtApplicationTests;
 import com.smtteam.smt.model.Activity;
 import com.smtteam.smt.service.ActivityService;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -12,14 +16,47 @@ import static org.junit.Assert.*;
  * created by Kimone
  * date 2019/1/20
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ActivityServiceImplTest extends SmtApplicationTests {
     @Autowired
     private ActivityService activityService;
 
+    private static Integer id;
+
     @Test
-    public void getActivityById() {
-        int id = 1;
-        Activity activity = activityService.getActivityById(id);
-        System.out.println(activity.toString());
+    public void test01_createActivity() {
+        Activity activity = new Activity(100,"test_create",1);
+        Activity activity1 = activityService.createActivity(activity);
+        id = activity1.getId();
+        assertNotNull(activity1.getId());
     }
+
+    @Test
+    public void test02_getActivityById() {
+        Activity activity = activityService.getActivityById(id);
+        assertEquals("test_create", activity.getName());
+    }
+
+    @Test
+    public void test03_modifyActivity() {
+        Activity activity = activityService.getActivityById(id);
+        activity.setName("test_modify");
+        Activity activity1 = activityService.modifyActivity(activity);
+        assertEquals("test_modify", activity1.getName());
+    }
+
+    @Test
+    public void test04_getActivityList() {
+        List<Activity> list = activityService.getActivityList(100);
+        assertEquals(1,list.size());
+    }
+
+    @Test
+    public void test05_deleteActivity() {
+        activityService.deleteActivity(id);
+        Activity activity = activityService.getActivityById(id);
+        assertNull(activity);
+    }
+
+
 }
